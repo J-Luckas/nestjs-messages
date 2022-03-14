019353fc683170a8c7bf9365b5ab53dffc09ee04
j-luckas <jairoluckas@hotmail.com>
2022-03-14 20:25:51 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -6,5 +6,6 @@ import { MessagesModule } from './messages/messages.module';
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
   await app.listen(3000);
+  
 }
 bootstrap();
